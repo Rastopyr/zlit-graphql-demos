24d1d650912f4245f32504c6bdfc486a8f999e80
Roman <rastopyr@gmail.com>
2019-05-05 12:02:12 +0300

    patch: start adding comment

---
+++ webhooks/src/index.js
@@ -0,0 +1,97 @@
+const yoga = require("graphql-yoga");
+const bodyParser = require("body-parser");
+
+const channel = "Rastopyr";
+const pubsub = new yoga.PubSub();
+
+const typeDefs = `
+  scalar JSON
+
+  type Query {
+    GithubWebhook(data: JSON): String
+  }
+
+  type Subscription {
+    issueComments: GithubComment!
+  }
+
+  type GithubComment {
+    id: Int
+    text: String
+    authorName: String
+  }
+`;
+
+const resolvers = {
+  GithubComment: {
+    id: (ghc) => ghc.id,
+    text: (ghc) => ghc.text,
+    authorName: (ghc) => ghc.authorName
+  },
+  Subscription: {
+    issueComments: {
+      subscribe: (commentPayload) => {
+        return pubsub.asyncIterator(channel);
+      }
+    }
+  },
+  Query: {
+    GithubWebhook: (_, { data }, { pubsub }) => {
+      if (data.comment) {
+        pubsub.publish(channel, {
+          issueComments: {
+            id: data.comment.id,
+            text: data.comment.body,
+            authorName: data.sender.name
+          }
+        });
+      }
+
+      return "200 OK";
+    }
+  }
+};
+
+const server = new yoga.GraphQLServer({
+  typeDefs,
+  resolvers,
+  context: {
+    pubsub
+  }
+});
+
+/*
+  Map payload of webhook to Github resolver
+*/
+const githubWebhook = (payload) => {
+  return {
+    query: `
+      query GithubHookHandle($payload: JSON) {
+        GithubWebhook(data: $payload)
+      }
+    `,
+    variables: {
+      payload
+    }
+  };
+};
+
+/*
+
+*/
+const handleWebhook = (hookFn) => (req, res, next) => {
+  if (!req.body) {
+    return next();
+  }
+
+  if (!req.body.query) {
+    req.body = hookFn(req.body);
+  }
+
+  next();
+};
+
+server.express.use(bodyParser.json());
+server.express.use(handleWebhook(githubWebhook));
+
+server.start({ port: 4566 }, () => console.log("listen at: localhost:4566"));
